a47f350c2eb527365bc901d84f96cbc5a83da754
hazbottles <harry.jack@outlook.com>
2020-10-06 14:14:34 +0100

    initial caching tests

---
+++ tests/test_caching.py
@@ -0,0 +1,52 @@
+import flonb
+
+
+def test_cache_disk():
+    _counts = [0]
+
+    @flonb.task_func(cache_disk=True)
+    def dummy_func():
+        _counts[0] += 1  # side effect - if cached, won't be executed
+
+    assert not dummy_func.cached
+    dummy_func.compute()
+    assert dummy_func.cached
+    dummy_func.compute()
+    assert _counts[0] == 1
+
+    dummy_func()  # call directly
+    assert _counts[0] == 2
+
+
+def test_cache_disk_with_parameters():
+    _add_one_xs = []
+    _multiply_ys = []
+
+    @flonb.task_func()
+    def add_one(x):
+        _add_one_xs.append(x)
+        return x + 1
+
+    @flonb.task_func(cache_disk=True)
+    def multiply(y, base=add_one):
+        _multiply_ys.append(y)
+        return base * y
+
+    @flonb.task_func(cache_disk=True)
+    def add_z(z, base=multiply):
+        return base + z
+
+    assert multiply.compute(x=3, y=2) == 8
+    assert multiply.compute(x=3, y=2) == 8  # cached result
+    assert multiply.compute(x=2, y=3) == 9
+    # checks that upstream parameter 'x' is included in cache identifiers
+    assert multiply.compute(x=5, y=3) == 18
+    # x=2 and y=2 have been computed separately
+    assert multiply.compute(x=2, y=2) == 6
+
+    assert add_z.compute(x=5, y=3, z=3) == 21  # cached result (x=5, y=3)
+    assert add_z.compute(x=5, y=3, z=7) == 25  # cached result (x=5, y=3)
+    assert add_z.compute(x=1, y=4, z=3) == 11  # new
+
+    assert _add_one_xs == [3, 2, 5, 2, 1]
+    assert _multiply_ys == [2, 3, 3, 2, 4]
