d088c217f138a4528899de0f19912aed2c32feeb
hazbottles <harry.jack@outlook.com>
2020-11-02 14:17:19 +0000

    version 0.1.0

--- flonb/__init__.py
+++ flonb/__init__.py
@@ -1,1 +1,5 @@
-from .task import task_func, Task, set_cache_dir, Dep, DynamicDep  # noqa: F401
+from .task import task_func, set_cache_dir, Dep, DynamicDep  # noqa: F401
+
+__version__ = "0.1.0"  # make sure to also update in ../setup.py
+
+__all__ = ["task_func", "set_cache_dir", "Dep", "DynamicDep", "__version__"]
