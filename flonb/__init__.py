bf491b269c0a3c48d4c4ebaba185175f35b40c42
Harry Jack <harry@solcast.com.au>
2024-07-01 02:16:14 +0000

    v0.1.3

--- flonb/__init__.py
+++ flonb/__init__.py
@@ -1,5 +1,5 @@
 from .task import task_func, set_cache_dir, Dep, DynamicDep  # noqa: F401
 
-__version__ = "0.1.2"  # make sure to also update in ../setup.py
+__version__ = "0.1.3"  # make sure to also update in ../setup.py
 
 __all__ = ["task_func", "set_cache_dir", "Dep", "DynamicDep", "__version__"]
